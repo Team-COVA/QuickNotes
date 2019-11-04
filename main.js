3e5a6b2c2cb085a020537b03e4df5811360c59b0
SCOTT CAHILL <SCAHILL97@YAHOO.COM>
2019-11-03 22:32:17 -0700

    ignore all previous changes until these everything is different

---
+++ main.js
@@ -0,0 +1,76 @@
+/*
+Initial JS File
+*/
+
+//https://stackoverflow.com/questions/247483/http-get-request-in-javascript
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function()
+    {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+        	//run the function provided as the callbacks
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous request
+    xmlHttp.send(null);
+}
+
+
+function requestCallback(responseText)
+{
+	/*
+	take the response text, and set it to the "serverTime"
+	element on the page so the user can see it
+	*/
+	let serverTimePElement = document.getElementById("servertime");
+	serverTimePElement.innerHTML = responseText;
+}
+
+var submit = document.getElementById("submit");
+var clear = document.getElementById("clear");
+var submitDate = new Date();
+
+
+submit.onclick = function buttonAction(){
+
+    var title = document.getElementById("title").value;
+    var content = document.getElementById("contents").value;
+    var data = {
+        'Title': title,
+        'Contents': content,
+        'Submit Date': submitDate
+        }
+
+    console.log(data);
+
+function dataPost(url){
+    var xhr = new XMLHttpRequest();
+    var url = '/submitNote';
+
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200);
+        }
+
+        xhr.open("GET", url, true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        var jsonData = JSON.stringify(data);
+        xhr.send(jsonData);
+
+}
+
+dataPost("http://localhost:5000/submitNote");
+
+
+}
+clear.onclick = function(){
+    document.getElementById("title").value = ""
+    document.getElementById("contents").value = ""
+}
+
+
+
+//submit request to server for the time when the page loads
+httpGetAsync("http://localhost:5000/getServerTime", requestCallback);
+
