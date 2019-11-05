3f1bf18e87cb902bd48a60e1d7a2f6a1558a1bcb
dtimmon1 <56605128+dtimmon1@users.noreply.github.com>
2019-11-04 22:39:09 -0500

    Save function
    Saving files to submitNote directory.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask import (Flask,
-render_template)
+render_template, request)
 
 import datetime
-
+import os.path
 
 def create_app():
     # create and configure the app
@@ -20,11 +20,37 @@ def create_app():
         message = "The current time on the server is: "
 
         #gets current time and formats it nicely
-        currentTime = datetime.datetime.now().strftime("%b %d %Y %H:%M:%S")
+        currentTime = datetime.datetime.now().strftime("%b %d %Y")
         message += str(currentTime) + "\n"
 
         #send the message as a response
         return message
+    @app.route('/submitNote', methods=['POST'])
+    def notePrint():
 
+        titleText = request.form["quicknote_title"]
+        contentText = request.form["qncontents"]
+        qnTitle = "Title: " + titleText
+        qnContent = "Content: " + contentText
 
+        # saves the date the note was created
+        createDate = datetime.datetime.now().strftime("%b %d %Y")
+        dateMessage = "QuickNote Created On: " + str(createDate)
+        # Open, Write, Save
+
+        save_path = ".\submitNote"
+        name_of_file = titleText
+        completeName = os.path.join(save_path, name_of_file + ".txt")
+        file1 = open(completeName, "w")
+        toFile = contentText # "Write what you want into the field"
+        file1.write(toFile)
+        file1.close()
+
+        # Unsure if this would work. Directory is referenced, but seems redundant.
+        return (qnTitle + '\n' + qnContent+ dateMessage)
+
+    # Very old iteration of saveFile. Disregard.
+    # def save('/submitNote', 'w'):
+    #    with open('/submitNote', 'w') as f:
+    #        f.write('contentText\n')
     return app
